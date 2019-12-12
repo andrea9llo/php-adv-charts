dd351b2c7148bbe7a9f3edd5775166d2d525a17d
andrea9llo <andrea.novello93@gmail.com>
2019-12-12 14:12:08 +0100

    step 1

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.1.2/handlebars.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment-with-locales.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/locale/it.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.bundle.js"></script>
+    <title>Grafici</title>
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+    <div class="container">
+      <div class="wrapper">
+        <canvas id="myChart"></canvas>
+      </div>
+    </div>
+
+
+    <script src="js/script.js" charset="utf-8"></script>
+
+  </body>
+</html>
---
+++ js/script.js
@@ -0,0 +1,36 @@
+function printData() {
+  $.ajax({
+    url:"server.php",
+    method:"GET",
+    success: function(data){
+      console.log(data);
+      genGraph(data);
+    },
+    error: function(error){
+      console.log("error",error);
+    }
+  });
+
+}
+
+function genGraph(elem) {
+  var ctx = document.getElementById('myChart').getContext('2d');
+  var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: moment.months(),
+        datasets: [{
+            label: 'vendite',
+            data:elem ,
+            backgroundColor: [
+                'rgba(0, 251, 34, 0.99)'
+            ],
+        }]
+    },
+});
+
+}
+
+$(document).ready(function(){
+  printData();
+});
